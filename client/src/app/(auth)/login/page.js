ab07614540c984fd11b238a9a5130df578fe4314
aaditya paudel <adityaspaudel@gmail.com>
2025-11-06 23:43:54 +0545

    css update

--- client/src/app/(auth)/login/page.js
+++ client/src/app/(auth)/login/page.js
@@ -67,10 +67,10 @@ const LoginForm = () => {
   });
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-green-100">
+    <div className="flex items-center justify-center min-h-screen bg-green-200">
       <form
         onSubmit={formik.handleSubmit}
-        className="w-full max-w-md p-6 bg-white rounded-lg shadow-md"
+        className="w-full max-w-md p-6 bg-white rounded-lg shadow shadow-gray-200 transition 3s hover:shadow-black"
       >
         <h2 className="mb-6 text-2xl font-bold text-center">Login</h2>
 
