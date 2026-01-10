e1fc91f3d677e3f52e0353d707dbd8b9abc66428
Aaditya Paudel <adityaspaudel@gmail.com>
2026-01-10 20:51:03 +0545

    skeleton loader added for profile, and message chat board component

--- client/src/app/(user)/[userId]/messages/page.js
+++ client/src/app/(user)/[userId]/messages/page.js
@@ -9,6 +9,7 @@ const UserMessageDashboard = () => {
 	const { userId } = useParams();
 	const [userDetails, setUserDetails] = useState(null);
 	const router = useRouter();
+	const [loading, setLoading] = useState(true);
 
 	const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL;
 	// alert(userId);
@@ -25,11 +26,45 @@ const UserMessageDashboard = () => {
 			console.error("could not fetch friends", error);
 		} finally {
 			console.log("");
+			setLoading(false);
 		}
 	}, [userId]);
 	useEffect(() => {
 		getUserDetails();
 	}, [getUserDetails]);
+
+	if (loading) {
+		return (
+			<main className="min-h-full bg-white overflow-auto text-black p-6 max-w-xl mx-auto h-full rounded-sm animate-pulse">
+				{/* Back Button */}
+				<div className="flex items-start text-2xl font-bold w-full">
+					<div className="h-8 w-8 bg-gray-300 rounded-2xl"></div>
+				</div>
+
+				{/* Title */}
+				<div className="h-7 w-64 bg-gray-300 rounded mt-4 mb-6"></div>
+
+				{/* User List */}
+				<div className="flex flex-col gap-3">
+					{Array.from({ length: 6 }).map((_, index) => (
+						<div
+							key={index}
+							className="flex items-center gap-4 bg-gray-100 p-2 rounded-md"
+						>
+							{/* Avatar */}
+							<div className="h-12 w-12 bg-gray-300 rounded-full"></div>
+
+							{/* User Info */}
+							<div className="flex flex-col gap-2 flex-1">
+								<div className="h-4 w-40 bg-gray-300 rounded"></div>
+								<div className="h-3 w-52 bg-gray-300 rounded"></div>
+							</div>
+						</div>
+					))}
+				</div>
+			</main>
+		);
+	}
 	return (
 		<main className="min-h-full bg-white overflow-auto text-black p-6 max-w-xl mx-auto   h-full rounded-sm">
 			<div
